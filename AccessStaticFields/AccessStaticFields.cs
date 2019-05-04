656dbcb79b862ae203775ebe9b51af8419f6bd8a
João Pedro Silveira <joao.pedro0842@hotmail.com>
2019-05-03 23:38:11 -0300

    Chapter 21

---
+++ AccessStaticFields/AccessStaticFields.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AccessStaticFields
+{
+    public partial class AccessStaticFields : Window
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var app = new Application();
+            app.Run(new AccessStaticFields());
+        }
+
+        public AccessStaticFields()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AccessStaticFields/Constants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace AccessStaticFields
+{
+    public static class Constants
+    {
+        public static readonly FontFamily fntfam = new FontFamily("Times New Roman Italic");
+
+        public static double FontSize => 72d / .75;
+
+        public static readonly LinearGradientBrush brush = new LinearGradientBrush(Colors.LightGray, Colors.DarkGray, new Point(), new Point(1, 1));
+    }
+}

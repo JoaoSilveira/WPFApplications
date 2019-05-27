9ae30d33cdcf324241c784c382ee58a79795b298
joao pedro silveira <Dr.Coka@hotmail.com>
2019-05-27 17:38:21 -0300

    Chapter 30 - Partial

---
+++ EnlargeButtonWithAnimation/EnlargeButtonWithAnimation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace EnlargeButtonWithAnimation
+{
+    public class EnlargeButtonWithAnimation : Window
+    {
+        const double initFontSize = 12;
+        const double maxFontSize = 48;
+        Button btn;
+
+        [STAThread]
+        public static void Main()
+        {
+            var app = new Application();
+            app.Run(new EnlargeButtonWithAnimation());
+        }
+
+        public EnlargeButtonWithAnimation()
+        {
+            Title = "Enlarge Button with Animation";
+
+            btn = new Button();
+            btn.Content = "Expanding Button";
+            btn.FontSize = initFontSize;
+            btn.HorizontalAlignment = HorizontalAlignment.Center;
+            btn.VerticalAlignment = VerticalAlignment.Center;
+            btn.Click += ButtonOnClick;
+            Content = btn;
+        }
+
+        private void ButtonOnClick(object sender, RoutedEventArgs e)
+        {
+            var anima = new DoubleAnimation();
+            anima.Duration = new Duration(TimeSpan.FromSeconds(2));
+            anima.From = initFontSize;
+            anima.To = maxFontSize;
+            anima.FillBehavior = FillBehavior.Stop;
+
+            btn.BeginAnimation(Button.FontSizeProperty, anima);
+        }
+    }
+}
---
+++ EnlargeButtonWithTimer/EnlargeButtonWithTimer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace EnlargeButtonWithTimer
+{
+    public class EnlargeButtonWithTimer : Window
+    {
+        const double initFontSize = 12;
+        const double maxFontSize = 48;
+        Button btn;
+
+        [STAThread]
+        public static void Main()
+        {
+            var app = new Application();
+            app.Run(new EnlargeButtonWithTimer());
+        }
+
+        public EnlargeButtonWithTimer()
+        {
+            Title = "Enlarge Button with Timer";
+
+            btn = new Button();
+            btn.Content = "Expanding Button";
+            btn.FontSize = initFontSize;
+            btn.HorizontalAlignment = HorizontalAlignment.Center;
+            btn.VerticalAlignment = VerticalAlignment.Center;
+            btn.Click += ButtonOnClick;
+            Content = btn;
+        }
+
+        private void ButtonOnClick(object sender, RoutedEventArgs e)
+        {
+            var tmr = new DispatcherTimer();
+            tmr.Interval = TimeSpan.FromSeconds(0.1);
+            tmr.Tick += TimerOnTick;
+            tmr.Start();
+        }
+
+        private void TimerOnTick(object sender, EventArgs e)
+        {
+            btn.FontSize += 2;
+
+            if (btn.FontSize >= maxFontSize)
+            {
+                btn.FontSize = initFontSize;
+                (sender as DispatcherTimer).Stop();
+            }
+        }
+    }
+}

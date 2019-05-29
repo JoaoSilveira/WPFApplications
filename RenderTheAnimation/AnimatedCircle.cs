cb891992f2718ae4e22226fc484481cda14f9269
João Pedro Silveira <joao.pedro0842@hotmail.com>
2019-05-28 21:22:56 -0300

    Chapter 30 - Partial

---
+++ RenderTheAnimation/AnimatedCircle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace RenderTheAnimation
+{
+    public class AnimatedCircle : FrameworkElement
+    {
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            var anima = new DoubleAnimation();
+            anima.From = 0;
+            anima.To = 100;
+            anima.Duration = new Duration(TimeSpan.FromSeconds(1));
+            anima.AutoReverse = true;
+            anima.RepeatBehavior = RepeatBehavior.Forever;
+
+            var clock = anima.CreateClock();
+            drawingContext.DrawEllipse(Brushes.Blue, new Pen(Brushes.Red, 3), new Point(125, 125), null, 0, clock, 0, clock);
+        }
+    }
+}
---
+++ RenderTheAnimation/RenderTheAnimation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace RenderTheAnimation
+{
+    public class RenderTheAnimation : Window
+    {
+        [STAThread]
+        public static void Main()
+        {
+            var app = new Application();
+            app.Run(new RenderTheAnimation());
+        }
+
+        public RenderTheAnimation()
+        {
+            Title = "Render the Animation";
+
+            Content = new AnimatedCircle();
+        }
+    }
+}
